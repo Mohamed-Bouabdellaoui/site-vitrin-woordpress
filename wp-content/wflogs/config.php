ccfb171d3b094def7b89444f7897b94736765c0f
mohamed <mohamedbouabdellaoui450@gmail.com>
2021-06-04 13:14:09 +0100

    site-vitrin-wordpress

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"zVKC^,OuSO+1-1,dv3hcZ#3T;uqju9W*~4N[K3&Xu{vo#QjK*;lmjpczOY$(dV?-";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1551;}
